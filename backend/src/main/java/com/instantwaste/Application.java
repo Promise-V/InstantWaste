ada764f4c264bffa44b2a1a7db499dba72247db7
Promise-V <promisevictor00@gmail.com>
2025-10-31 13:41:13 -0400

    controller debugging

--- backend/src/main/java/com/instantwaste/Application.java
+++ backend/src/main/java/com/instantwaste/Application.java
@@ -2,8 +2,10 @@ package com.instantwaste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.instantwaste")
 public class Application {
 
     public static void main(String[] args) {
